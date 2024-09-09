aed02bf4f6ff1563e7f1a0fb31ccfa6ceff52dc0
Sakthi Ganapathi <110651198+sakthi-sakthi@users.noreply.github.com>
2024-09-09 15:05:28 +0530

    Update Url.js

--- src/dashboard/pages/Api/Url.js
+++ src/dashboard/pages/Api/Url.js
@@ -1,3 +1,3 @@
-// const AppUrl = "https://religioapp.cristoerp.com";
-const AppUrl = "http://127.0.0.1:8000/api";
+const AppUrl = "https://religioapp.cristoerp.com/api";
+// const AppUrl = "http://127.0.0.1:8000/api";
 export default AppUrl;
